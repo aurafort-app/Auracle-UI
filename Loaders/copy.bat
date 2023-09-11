8529803178e34c1ceacc4575f25118b326541dc5
Sri Ranga Bharadwaj Chakilam <chakilamsriranga@gmail.com>
2023-09-11 21:24:05 +0530

    spin loaders

---
+++ Loaders/copy.bat
@@ -0,0 +1,16 @@
+@echo off
+
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_17.html /y
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_18.html /y
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_19.html /y
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_20.html /y
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_21.html /y
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_22.html /y
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_23.html /y
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_24.html /y
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_25.html /y
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_26.html /y
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_27.html /y
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_28.html /y
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_29.html /y
+xcopy D:\Projects\Auracle-UI\Loaders\loader_spinner_1.html D:\Projects\Auracle-UI\Loaders\loader_spinner_30f.html /y
